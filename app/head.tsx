da9abbe4e32db78a9d4fb7d9eb901d3dc5e139fb
Drago Vukovic <gudraliin@gmail.com>
2023-03-29 22:08:33 +0200

    head element

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head(
+) {
+    return (
+        <head>
+            <meta name="viewport" content="width=device-width" />
+            <title>Document title</title>
+        </head>
+    )
+}
