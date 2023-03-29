677d755af2d546266621fd3aa474ea4c1b50a84c
Drago Vukovic <gudraliin@gmail.com>
2023-03-29 23:09:22 +0200

    S

--- components/TableGalerija.tsx
+++ components/TableGalerija.tsx
@@ -5,13 +5,13 @@ export default function TableGalerija() {
         <div className="mt-20 bg-slate-50 rounded-xl  ">
             <h1 className="text-center font-bold text-xl p-2 text-slate-500">Kako nasa proizvodnja izgleda</h1>
             <div className=" p-4 flex-wrap flex justify-center items-center gap-6">
-                <Image className=" flex-1 transition duration-300 ease-in-out hover:scale-150 " width={200} height={200} src={"/img_3016.jpg"} alt={"slika masine"} />
-                <Image className="flex-1  transition duration-300 ease-in-out hover:scale-150 " width={200} height={200} src={"/img_3017.jpg"} alt={"slika masine"} />
-                <Image className="flex-1  transition duration-300 ease-in-out hover:scale-150 " width={200} height={200} src={"/img_3018.jpg"} alt={"slika masine"} />
+                <Image className=" flex-1 transition duration-300 ease-in-out hover:scale-150 " width={200} height={200} src={"/IMG_3016.JPG"} alt={"slika masine"} />
+                <Image className="flex-1  transition duration-300 ease-in-out hover:scale-150 " width={200} height={200} src={"/IMG_3017.JPG"} alt={"slika masine"} />
+                <Image className="flex-1  transition duration-300 ease-in-out hover:scale-150 " width={200} height={200} src={"/IMG_3018.JPG"} alt={"slika masine"} />
 
-                <Image className="flex-1  transition duration-300 ease-in-out hover:scale-150 " width={200} height={200} src={"/img_3015.jpg"} alt={"slika masine"} />
-                <Image className="  transition duration-300 ease-in-out hover:scale-75 " width={200} height={200} src={"/img_2991.jpg"} alt={"slika masine"} />
-                <Image className="  transition duration-300 ease-in-out hover:scale-75 " width={200} height={200} src={"/img_2992.jpg"} alt={"slika masine"} />
+                <Image className="flex-1  transition duration-300 ease-in-out hover:scale-150 " width={200} height={200} src={"/IMG_3015.JPG"} alt={"slika masine"} />
+                <Image className="  transition duration-300 ease-in-out hover:scale-75 " width={200} height={200} src={"/IMG_2991.JPG"} alt={"slika masine"} />
+                <Image className="  transition duration-300 ease-in-out hover:scale-75 " width={200} height={200} src={"/IMG_2992.JPG"} alt={"slika masine"} />
 
             </div>
 
