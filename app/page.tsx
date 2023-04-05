669bf466114a176cfe1d91584662031336c75f97
Drago Vukovic <gudraliin@gmail.com>
2023-04-05 14:12:47 +0200

    swiper

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,9 @@ export default function Home() {
         <Hero />
         {  /* <MyComponent /> */}
         <Partneri />
-        <Proizvodi />
+        <div className=''>
+          <Proizvodi />
+        </div>
         <NestoONama />
         <ZastoNasKontakt />
         <Misija />
--- components/Proizvodi.tsx
+++ components/Proizvodi.tsx
@@ -1,53 +1,51 @@
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/effect-cube';
-import 'swiper/css/effect-fade';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
+import SwiperCore, { Autoplay } from 'swiper';
 interface Slide {
     image: string;
     alt: string;
 }
+SwiperCore.use([Autoplay]);
 
 const Proizvodi: React.FC = () => {
     const slides: Slide[] = [
-        { image: '/IMG_2991.JPG', alt: 'Slide 1' },
+
         { image: '/IMG_2991.JPG', alt: 'Slide 2' },
-        { image: '/IMG_2991.JPG', alt: 'Slide 3' },
-        { image: '/IMG_2991.JPG', alt: 'Slide 4' },
-        { image: '/IMG_2991.JPG', alt: 'Slide 5' },
-        { image: '/IMG_2991.JPG', alt: 'Slide 6' },
-        { image: '/IMG_2991.JPG', alt: 'Slide 7' },
-        { image: '/IMG_2991.JPG', alt: 'Slide 8' },
-        { image: '/IMG_2991.JPG', alt: 'Slide 9' },
-        { image: '/IMG_2991.JPG', alt: 'Slide 10' },
+        { image: '/PHOTO-2023-03-30-11-24-07.png', alt: 'Slide 4' },
+        { image: '/PHOTO-2023-03-30-11-29-38.png', alt: 'Slide 6' },
+        { image: '/PHOTO-2023-03-30-11-29-41 2.png', alt: 'Slide 7' },
+        { image: '/PHOTO-2023-03-30-11-29-41 3.png', alt: 'Slide 8' },
+        { image: '/PHOTO-2023-03-30-11-29-41.png', alt: 'Slide 10' },
     ];
 
     const params = {
 
         loop: true,
-        slidesPerView: 3,
+        slidesPerView: 4,
         autoplay: {
-            delay: 3000,
+            delay: 300,
             disableOnInteraction: true,
         },
     };
 
     return (
-        <div>
+        <div className=''>
             <h2 className="text-center text-xl italic font-semibold sm:text-2xl p-4">
                 Pogledajte primere nekih ambalaza koje pravimo
             </h2>
-            <div className="swiper-container p-20">
+            <div className="swiper-container sm:px-64 py-20">
                 <Swiper {...params}>
                     {slides.map((slide, index) => (
-                        <SwiperSlide key={index} className='swiper-slide'>
-                            <img src={slide.image} width={200} height={200} alt={slide.alt} />
+                        <SwiperSlide key={index} className="swiper-slide">
+                            <img src={slide.image} width={300} height={400} alt={slide.alt} />
                         </SwiperSlide>
                     ))}
                 </Swiper>
+
             </div>
         </div>
     );
